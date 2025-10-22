169048a56f2a341779f4ea0b92ec0f3d43c25c49
ALUNO C33 <alunoc33@unifor.local>
2025-10-21 21:06:03 -0300

    Adicionado outras rotas

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ app.post('/resultado', (req, res) => {
     res.send(`<h2>O resultado de ${meses} trabalhados com salário bruto de R$${salario} = R$${resultado.toFixed(2)}</h2><br><a href="/">Voltar</a>`)
 })
 
+app.get('/salario/:valor', (req, res) => {
+    const valor = req.params.valor;
+
+    res.send(`<h1>O seu salario líquido é R$${valor}</h1>`)
+})
+
+app.get('/funcionario', (req, res) => {
+    res.send("<h1>Seja bem vindo funcionario</h1>")
+})
+
 console.log("PORT env: ", process.env.port);
 
 app.listen(PORT, () => {
